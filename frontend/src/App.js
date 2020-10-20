f2ee5d5160122f21e2da7bc1a632861ff8a8c234
comradeMauser <60297848+comradeMauser@users.noreply.github.com>
2020-10-20 23:42:20 +0300

    redux; npm redux, react-redux, redux-thunk, redux-devtools-extension; create redux store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,9 @@ import {Container} from 'react-bootstrap';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
-import About from "./screens/About";
-import HomeScreen from "./screens/HomeScreen";
-import ProductScreen from "./screens/ProductScreen";
+import About from './screens/About';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 
 const App = () => {
