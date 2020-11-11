4d9e52d40af3c448e193454fb5fffe2b8c7fbef2
comradeMauser <singularquantumamoeba@gmail.com>
2020-11-11 23:02:38 +0300

    backend authentication; Authentication middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -28,20 +28,17 @@ export const authUser = asyncHandler(async (request, response) => {
 // route            GET /api/users/profile
 // access           Private
 export const getUserProfile = asyncHandler(async (request, response) => {
-    response.send("success")
-    /*    const user = await User.findById(request.user._id)
+    const user = await User.findById(request.user._id)
 
-        if (user && await user.matchPassword(password)) {
-            response.json({
-                _id: user._id,
-                name: user.name,
-                email: user.email,
-                isAdmin: user.isAdmin,
-                token: generateToken(user._id),
-            })
-        } else {
-            response.status(401)
-            throw new Error('You shall not pass! *(wrong email or password)')
-        }*/
+    if (user) {
+        response.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else {
+        response.status(401)
+        throw new Error("User not found")
+    }
 });
-
---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
+
+
+export const protect = asyncHandler(async (request, response, next) => {
+    let token
+
+    if (request.headers.authorization && request.headers.authorization.startsWith('Bearer')) {
+        try {
+            token = request.headers.authorization.split(' ')[1]
+            console.log("token:", token)
+
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+            console.log("decoded:", decoded)
+
+            request.user = await User.findById(decoded.id).select("-password")
+        } catch (e) {
+            console.error(e)
+            response.status(401)
+            throw new Error("Not Authorization, token failed")
+        }
+    }
+    if (!token) {
+        response.status(401)
+        throw new Error("Not authorized, no token")
+    }
+
+    console.log("Hi asshole")
+    next()
+})
