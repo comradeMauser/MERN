c7aedd578d24b1b04c0acde8790ea8b400601d13
comradeMauser <singularquantumamoeba@gmail.com>
2020-11-13 14:19:13 +0300

    backend authentication; user registration, password encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ import asyncHandler from 'express-async-handler';
 import User from '../models/userModel.js';
 import generateToken from '../utils/generateToken.js'
 
+
 // description      Authentication user & get token
 // route            POST /api/users/login
 // access           Public
@@ -22,6 +23,38 @@ export const authUser = asyncHandler(async (request, response) => {
         response.status(401)
         throw new Error('You shall not pass! *(wrong email or password)')
     }
+});
+
+// description      Register new user
+// route            POST /api/users
+// access           Public
+export const registerUser = asyncHandler(async (request, response) => {
+    const {name, email, password} = request.body
+
+    const userExists = await User.findOne({email})
+
+    if (userExists) {
+        response.status(400)
+        throw new Error("User already exists")
+    }
+
+    const user = await User.create({
+        name, email, password
+    })
+
+    if (user) {
+        response.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+        })
+    } else {
+        response.status(400)
+        throw new Error("Invalid User data")
+    }
+
 });
 
 // description      Get user profile
